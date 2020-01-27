40a1963dae3427a37eced6eecb60eb2e93e686e1
Phan Khac Dao <daofresh@gmail.com>
2020-01-27 13:20:28 +0700

    Updates

---
+++ precache-manifest.15287bc6c95f23c6b04f6af25193344d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4174f9a4099a5048c125",
+    "url": "/css/app.48ae630f.css"
+  },
+  {
+    "revision": "4432cc01a43e52b9fc84",
+    "url": "/css/chunk-88766cfe.9e323ab1.css"
+  },
+  {
+    "revision": "353938e53954694dab6e",
+    "url": "/css/chunk-vendors.340b80bc.css"
+  },
+  {
+    "revision": "4647067baa6a0166b76a85f7e71ba5bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4174f9a4099a5048c125",
+    "url": "/js/app.2836a817.js"
+  },
+  {
+    "revision": "4432cc01a43e52b9fc84",
+    "url": "/js/chunk-88766cfe.e6a91241.js"
+  },
+  {
+    "revision": "353938e53954694dab6e",
+    "url": "/js/chunk-vendors.2da7b73a.js"
+  },
+  {
+    "revision": "abf92039df76d5ccf7ce8a3459a3abbc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
